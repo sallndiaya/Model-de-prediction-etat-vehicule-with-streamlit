bd3e737cd57da6a6fc88118326e680c8426df52d
ndiaya <100203460+sallndiaya@users.noreply.github.com>
2026-09-17 20:39:40 +0000

    Update app_car.py

--- app_car.py
+++ app_car.py
@@ -32,7 +32,7 @@ def load_artifacts():
     encoders = jb.load("encoders.joblib")   # encodeurs (Marque, Quartier, Transmission)
     uniques = jb.load("uniques.joblib")     # valeurs uniques
     scaler = jb.load("scaler.joblib")       # normaliseur
-    xgb = jb.load("svm_model.joblib")       # modèle
+    svm = jb.load("svm_model.joblib")       # modèle
     return encoders, uniques, scaler, svm
 
 
